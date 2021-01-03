aecbc3bc162665f707015c78924a90161b4dd8fd
JosiasAurel <ndjosiasaurel@gmail.com>
2021-01-03 17:10:40 +0100

    create game

---
+++ enemy.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	var Player = get_parent().get_node("player")
+	
+	position += (Player.position - position)/50
+	look_at(Player.position)
+	
+	move_and_slide(motion)
+
+
+func _on_Area2D_body_entered(body):
+	if "Bullet" in body.name:
+		queue_free()
